6ee7b8b2d36964f032c3c6e4b9bc828ecefbea6a
ianmottert <motterti@gmail>
2021-09-16 20:04:19 -0500

    Finished without bonus

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -3,7 +3,7 @@ package main;
 
 public class BalancedBrackets {
     /**
-     * The function BalancedBrackets should return true if and only if
+     * The function hasBalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
      *
      * That is, whether it consists entirely of pairs of opening/closing
@@ -24,7 +24,7 @@ public class BalancedBrackets {
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
         for (char ch : str.toCharArray()) {
-            if (ch == '[') {
+            if (ch == '[' && brackets != -1) {
                 brackets++;
             } else if (ch == ']') {
                 brackets--;
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,64 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void bracketsInFrontReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void wordInBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void onlyBeginningBracketInWordReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void bracketsOutOfOrderInWordReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void onlyBeginningBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyEndBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void OnlyBracketsOutOfOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void tooManyOpeningBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+    }
+
+    @Test
+    public void tooManyOpeningClosedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][]"));
+    }
+
+    @Test
+    public void NestedBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
 
 }
